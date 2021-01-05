e949cfe83dcda341a2f2bff4a1efb9f04b4d5749
kpradeep12 <k.pradeep12@outlook.com>
2021-01-04 22:52:18 -0500

    spring security section two

---
+++ spring-security-user-service-2/src/main/java/net/thetechstack/springsecurity/config/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package net.thetechstack.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsService();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
