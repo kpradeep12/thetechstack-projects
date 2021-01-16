dc43df8cf7d46bc90f9f604c1fd7a69dd636729f
Pradeep <kpradeep12@users.noreply.github.com>
2021-01-16 10:14:47 -0500

    new project

---
+++ project-my-book-collection/src/main/java/net/thetechstack/projectmybookcollection/ProjectMyBookCollectionApplication.java
@@ -0,0 +1,13 @@
+package net.thetechstack.projectmybookcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMyBookCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMyBookCollectionApplication.class, args);
+	}
+
+}
