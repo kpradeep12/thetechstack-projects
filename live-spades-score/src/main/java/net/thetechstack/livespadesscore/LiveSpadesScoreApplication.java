30b03520ca661f8ec416ed232e38b145e99764ab
kpradeep12 <k.pradeep12@outlook.com>
2021-07-11 11:58:40 -0400

    new project live-spades-score added

---
+++ live-spades-score/src/main/java/net/thetechstack/livespadesscore/LiveSpadesScoreApplication.java
@@ -0,0 +1,13 @@
+package net.thetechstack.livespadesscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveSpadesScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveSpadesScoreApplication.class, args);
+	}
+
+}
---
+++ live-spades-score/src/test/java/net/thetechstack/livespadesscore/LiveSpadesScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package net.thetechstack.livespadesscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveSpadesScoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
